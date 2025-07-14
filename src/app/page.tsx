0bd0b6745ac262713ae05949e6fb3bfec98cccab
Abigail <abigail.addo@bsystemslimited.com>
2025-07-14 17:54:23 +0000

    Installed and configured tailwind css and mui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+"use client"
+
+import * as React from 'react';
+
+
 export default function Home() {
   return (
     <>
-      <h1>Welcome</h1>
+      <h1 className="text-red-500 border border-cyan-400 font-extrabold text-2xl">Welcome</h1>
     </>
   );
 }
