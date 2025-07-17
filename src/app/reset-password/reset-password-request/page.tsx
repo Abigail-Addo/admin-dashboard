9bbc0d8bca90f4091ebb15217c41d54146938df0
Abigail <abigail.addo@bsystemslimited.com>
2025-07-17 06:18:06 +0000

    reset password page added

--- src/app/reset-password/reset-password-request/page.tsx
+++ src/app/reset-password/reset-password-request/page.tsx
@@ -1,13 +1,198 @@
 "use client"
 
 import * as React from 'react';
+import Image from 'next/image';
+import { Controller, SubmitHandler, useForm } from 'react-hook-form';
+import { FormControl, IconButton, InputAdornment, InputLabel, OutlinedInput } from '@mui/material';
+import { FaRegEye, FaRegEyeSlash } from 'react-icons/fa';
+import Link from 'next/link';
 
-
+interface IFormInput {
+    id: string | number;
+    newPassword: string;
+    confirmPassword: string;
+}
 const ResetPasswordRequest = () => {
+    const { control, handleSubmit, formState: { errors } } = useForm<IFormInput>({
+        defaultValues: {
+            newPassword: '',
+            confirmPassword: '',
+        }
+    })
+
+    const [showPassword, setShowPassword] = React.useState(false);
+    const [showConfirmPassword, setShowConfirmPassword] = React.useState(false);
+
+    // function to reset password
+    const onSubmit: SubmitHandler<IFormInput> = async () => {
+        try {
+            console.log("clicked")
+
+        } catch (error) {
+            console.log(error)
+        }
+    };
 
     return (
         <>
-            <h1 className="text-red-500 border border-cyan-400 font-extrabold text-2xl">Welcome to the reset password resquest page</h1>
+            <main className='w-screen h-screen relative'>
+                {/* hero image */}
+                <div className="absolute inset-0 -z-10 hidden lg:flex">
+                    <Image
+                        src="/assets/hero.png"
+                        alt="hero"
+                        layout="fill"
+                        objectFit="cover"
+                        className="w-full h-full"
+                    />
+                    <div className="absolute inset-0 bg-[#D9D9D9] opacity-30" />
+                </div>
+
+                {/* form */}
+                <div className="flex justify-center items-center h-full w-full px-4 overflow-y-scroll">
+                    <div className='h-fit lg:w-2/6 bg-white rounded-lg lg:shadow-lg overflow-y-scroll px-6 py-8 md:px-8 md:py-10'>
+                        <form
+                            onSubmit={handleSubmit(onSubmit)}
+                            className=" w-full space-y-6"
+                        >
+
+                            {/* logo */}
+                            <div className="flex justify-center">
+                                {/* Small screen logo */}
+                                <Image
+                                    src="/logo/logo-mobile.png"
+                                    alt="Logo Small"
+                                    width={100}
+                                    height={40}
+                                    className="block md:hidden"
+                                />
+                                {/* Medium screen logo */}
+                                <Image
+                                    src="/logo/logo-tablet.png"
+                                    alt="Logo Medium"
+                                    width={120}
+                                    height={50}
+                                    className="hidden md:block lg:hidden"
+                                />
+                                {/* Large screen logo */}
+                                <Image
+                                    src="/logo/logo-tablet.png"
+                                    alt="Logo Large"
+                                    width={140}
+                                    height={60}
+                                    className="hidden lg:block"
+                                />
+                            </div>
+
+                            {/* title */}
+                            <div className="text-center">
+                                <h2 className="text-xl font-bold text-gray-800 md:text-2xl">Admin Reset Password</h2>
+                                <p className="text-sm text-gray-600 mt-1">
+                                    Create new password
+                                </p>
+                            </div>
+
+                            {/* Password Field */}
+                            <div>
+                                <Controller
+                                    name="newPassword"
+                                    control={control}
+                                    rules={{ required: "Password is required" }}
+                                    render={({ field }) => (
+                                        <FormControl fullWidth variant="outlined" size='small'
+                                            sx={{
+                                                '& .MuiInputLabel-root.Mui-focused ': {
+                                                    color: '#01589A',
+                                                },
+                                                '& .MuiOutlinedInput-root': {
+                                                    '&:hover fieldset': {
+                                                        borderColor: '#01589A',
+                                                    },
+                                                    '&.Mui-focused fieldset': {
+                                                        borderColor: '#01589A',
+                                                    },
+                                                },
+                                            }}
+                                        >
+                                            <InputLabel>Password</InputLabel>
+                                            <OutlinedInput
+                                                {...field}
+                                                type={showPassword ? "text" : "password"}
+                                                endAdornment={
+                                                    <InputAdornment position="end">
+                                                        <IconButton onClick={() => setShowPassword(!showPassword)}>
+                                                            {showPassword ? <FaRegEyeSlash /> : <FaRegEye />}
+                                                        </IconButton>
+                                                    </InputAdornment>
+                                                }
+                                                label="Password"
+                                            />
+                                        </FormControl>
+                                    )}
+                                />
+                                {errors.newPassword && <p className="text-red-500 text-sm">{errors.newPassword.message}</p>}
+                            </div>
+
+                            {/* Confirm Password Field */}
+                            <div>
+                                <Controller
+                                    name="confirmPassword"
+                                    control={control}
+                                    rules={{
+                                        required: "Confirm your password",
+                                        // validate: value => value === password || "Passwords do not match"
+                                    }}
+                                    render={({ field }) => (
+                                        <FormControl fullWidth variant="outlined" size='small'
+                                            sx={{
+                                                '& .MuiInputLabel-root.Mui-focused ': {
+                                                    color: '#01589A',
+                                                },
+                                                '& .MuiOutlinedInput-root': {
+                                                    '&:hover fieldset': {
+                                                        borderColor: '#01589A',
+                                                    },
+                                                    '&.Mui-focused fieldset': {
+                                                        borderColor: '#01589A',
+                                                    },
+                                                },
+                                            }}
+                                        >
+                                            <InputLabel>Confirm Password</InputLabel>
+                                            <OutlinedInput
+                                                {...field}
+                                                type={showConfirmPassword ? "text" : "password"}
+                                                endAdornment={
+                                                    <InputAdornment position="end">
+                                                        <IconButton onClick={() => setShowConfirmPassword(!showConfirmPassword)}>
+                                                            {showConfirmPassword ? <FaRegEyeSlash /> : <FaRegEye />}
+                                                        </IconButton>
+                                                    </InputAdornment>
+                                                }
+                                                label="Confirm Password"
+                                            />
+                                        </FormControl>
+                                    )}
+                                />
+                            </div>
+
+                            {/* button */}
+                            <div className="w-full flex justify-center">
+                                <button
+                                    type='submit'
+                                    className="bg-[#01589A] text-white px-5 py-2 rounded hover:bg-[#014273] focus:bg-[#01589a] w-full"
+                                >
+                                    Reset password
+                                </button>
+                            </div>
+
+                            <p className='text-center'>Back to homepage,
+                                <Link href="/reset-password-request" className='text-[#01589A]'>Back</Link>
+                            </p>
+                        </form>
+                    </div>
+                </div >
+            </main >
         </>
     );
 }
