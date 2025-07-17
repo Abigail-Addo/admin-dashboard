ecab0e8739e056d746f79a5177b727e00214a823
Abigail <abigail.addo@bsystemslimited.com>
2025-07-17 06:03:14 +0000

    Login page added

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,13 +1,198 @@
 "use client"
 
 import * as React from 'react';
+import Image from 'next/image';
+import { Controller, SubmitHandler, useForm } from 'react-hook-form';
+import { FormControl, IconButton, InputAdornment, InputLabel, OutlinedInput, TextField } from '@mui/material';
+import { FaRegEye, FaRegEyeSlash } from 'react-icons/fa';
+import Link from 'next/link';
 
-
+interface IFormInput {
+    id: string | number;
+    email: string;
+    password: string;
+}
 const Login = () => {
+    const { control, handleSubmit, formState: { errors } } = useForm<IFormInput>({
+        defaultValues: {
+            email: '',
+            password: '',
+        }
+    })
+
+    const [showPassword, setShowPassword] = React.useState(false);
+
+    // function to log in an admin
+    const onSubmit: SubmitHandler<IFormInput> = async () => {
+        try {
+            console.log("clicked")
+
+        } catch (error) {
+            console.log(error)
+        }
+    };
 
     return (
         <>
-            <h1 className="text-red-500 border border-cyan-400 font-extrabold text-2xl">Welcome to the Login page</h1>
+            <main className='w-screen h-screen relative'>
+                {/* hero image */}
+                <div className="absolute inset-0 -z-10 hidden lg:flex">
+                    <Image
+                        src="/assets/hero.png"
+                        alt="hero"
+                        layout="fill"
+                        objectFit="cover"
+                        className="w-full h-full"
+                    />
+                    <div className="absolute inset-0 bg-[#D9D9D9] opacity-30" />
+                </div>
+
+                {/* form */}
+                <div className="flex justify-center items-center h-full w-full px-4 overflow-y-scroll">
+                    <div className='h-fit bg-white rounded-lg lg:shadow-lg overflow-y-scroll px-6 py-8 md:px-8 md:py-10'>
+                        <form
+                            onSubmit={handleSubmit(onSubmit)}
+                            className=" w-full space-y-6"
+                        >
+
+                            {/* logo */}
+                            <div className="flex justify-center">
+                                {/* Small screen logo */}
+                                <Image
+                                    src="/logo/logo-mobile.png"
+                                    alt="Logo Small"
+                                    width={100}
+                                    height={40}
+                                    className="block md:hidden"
+                                />
+                                {/* Medium screen logo */}
+                                <Image
+                                    src="/logo/logo-tablet.png"
+                                    alt="Logo Medium"
+                                    width={120}
+                                    height={50}
+                                    className="hidden md:block lg:hidden"
+                                />
+                                {/* Large screen logo */}
+                                <Image
+                                    src="/logo/logo-tablet.png"
+                                    alt="Logo Large"
+                                    width={140}
+                                    height={60}
+                                    className="hidden lg:block"
+                                />
+                            </div>
+
+                            {/* title */}
+                            <div className="text-center">
+                                <h2 className="text-xl font-bold text-gray-800 md:text-2xl">Admin Login</h2>
+                                <p className="text-sm text-gray-600 mt-1">
+                                    Login to Manage and Access the Dashboard Effortlessly.
+                                </p>
+                            </div>
+
+                            {/* email */}
+                            <div>
+                                <Controller
+                                    name="email"
+                                    control={control}
+                                    rules={{
+                                        required: "Please enter a valid email",
+                                        pattern: {
+                                            value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                                            message: "Invalid email address",
+                                        }
+                                    }}
+                                    render={({ field }) =>
+                                        <TextField
+                                            {...field}
+                                            id="outlined-basic"
+                                            label="Email"
+                                            variant="outlined"
+                                            className="w-full"
+                                            autoComplete="off"
+                                            type="text"
+                                            size='small'
+                                            sx={{
+                                                '& .MuiInputLabel-root.Mui-focused ': {
+                                                    color: '#01589A',
+                                                },
+                                                '& .MuiOutlinedInput-root': {
+                                                    '&:hover fieldset': {
+                                                        borderColor: '#01589A',
+                                                    },
+                                                    '&.Mui-focused fieldset': {
+                                                        borderColor: '#01589A',
+                                                    },
+                                                },
+                                            }}
+                                        />
+                                    }
+                                />
+                                {errors.email && (
+                                    <p role="alert" className="text-red-500 text-sm">{errors.email.message}</p>
+                                )}
+                            </div>
+
+                            {/* Password Field */}
+                            <div>
+                                <Controller
+                                    name="password"
+                                    control={control}
+                                    rules={{ required: "Password is required" }}
+                                    render={({ field }) => (
+                                        <FormControl fullWidth variant="outlined" size='small'
+                                            sx={{
+                                                '& .MuiInputLabel-root.Mui-focused ': {
+                                                    color: '#01589A',
+                                                },
+                                                '& .MuiOutlinedInput-root': {
+                                                    '&:hover fieldset': {
+                                                        borderColor: '#01589A',
+                                                    },
+                                                    '&.Mui-focused fieldset': {
+                                                        borderColor: '#01589A',
+                                                    },
+                                                },
+                                            }}
+                                        >
+                                            <InputLabel>Password</InputLabel>
+                                            <OutlinedInput
+                                                {...field}
+                                                type={showPassword ? "text" : "password"}
+                                                endAdornment={
+                                                    <InputAdornment position="end">
+                                                        <IconButton onClick={() => setShowPassword(!showPassword)}>
+                                                            {showPassword ? <FaRegEyeSlash /> : <FaRegEye />}
+                                                        </IconButton>
+                                                    </InputAdornment>
+                                                }
+                                                label="Password"
+                                            />
+                                        </FormControl>
+                                    )}
+                                />
+                                {errors.password && <p className="text-red-500 text-sm">{errors.password.message}</p>}
+                                <Link href='/forgot-password' className='text-[#01589A]'>forgot password?</Link>
+                            </div>
+
+                            {/* button */}
+                            <div className="w-full flex justify-center">
+                                <button
+                                    type='submit'
+                                    className="bg-[#01589A] text-white px-5 py-2 rounded hover:bg-[#014273] focus:bg-[#01589a] w-full"
+                                >
+                                    Login
+                                </button>
+                            </div>
+
+                            <p className='text-center'>Don&apos;t have an account yet?
+                                <Link href="/signup" className='text-[#01589A]'>Signup</Link>
+                            </p>
+                        </form>
+                    </div>
+                </div >
+            </main >
         </>
     );
 }
